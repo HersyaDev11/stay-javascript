04d01f48619e782b21cab74fd79a8a5651972e21
HersyaDev11 <Hersyayukione@gmail.com>
2024-10-05 21:41:03 +0700

    first commit

---
+++ Hello.js
@@ -0,0 +1,7 @@
+var a = 2;
+
+if (a === 2){
+   console.log('Perintah 1')
+   console.log('Perintah 2')
+   
+}
---
+++ const.js
@@ -0,0 +1,12 @@
+const PI = 3.18585626564757;
+
+var radius =5.0;
+
+var luas, keliling;
+
+luas = PI * radius * radius;
+
+keliling = 2 * PI * radius;
+
+console.log('Luas lingkaran\t\t: ' + luas);
+console.log('Keliling lingkaran\t: ' + keliling);
